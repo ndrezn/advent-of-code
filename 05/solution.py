c79ee487315335854a53cfca85408d9f281dd9c7
Nathan Drezner <nathan@drezner.xyz>
2022-12-05 20:05:13 -0500

    Use a recursive solution; clean up

--- 05/solution.py
+++ 05/solution.py
@@ -2,44 +2,36 @@ import numpy as np
 import regex as re
 
 
-def get_top_layer(config):
-    return "".join([i.pop() for i in config])
+def move_crates(config, instructions, reverse):
+    if not instructions:
+        return config
 
-
-def move_crate(config, count, start_col, end_col, reverse=False):
-    moved_boxes = []
-    start_col -= 1
-    end_col -= 1
-    for i in range(count):
-        moved_boxes.append(config[start_col].pop())
-
+    count, start_col, end_col = instructions.pop(0)
+    moved_boxes = [config[start_col - 1].pop() for i in range(count)]
+    # Reversing simulates "move a bunch of boxes in one grab"
     moved_boxes = reversed(moved_boxes) if reverse else moved_boxes
+    config[end_col - 1] += moved_boxes
 
-    for i in moved_boxes:
-        config[end_col].append(i)
-
-    return config
+    return move_crates(config, instructions, reverse)
 
 
 def get_metadata(f):
-    # Get the contents. Break on newline to split movement instructions
-    # from configuration
+    # Get the contents. Break on newline to split config from instructions
     config, instructions = open(f, "r").read().rstrip().split("\n\n")
-
     config = config.split("\n")[:-1]
 
     def fix_row(row):
         # ChatGPT with a helping hand constructing this regex 😉
         regex = r" (   )|\[(.*?)\]"
-
         matches = re.findall(regex, row)
         return [i[1].strip() for i in matches]
 
-    config = list(map(fix_row, config))
-
-    config = np.rot90(np.array(config), k=3).tolist()
-
-    # Clear out our placeholder characters required to keep all columns the same size during the rotation
+    # Convert each row to a proper python.
+    # e.g. "    [D]    " becomes ['', 'D', '']
+    config = map(fix_row, config)
+    # Rotate our rows 270º so we get our columns; useful for push/pop
+    config = np.rot90(np.array(list(config)), k=3).tolist()
+    # Clear out blank characters required to keep all columns the same size for rotation
     config = [[i for i in col if i] for col in config]
 
     instructions = [
@@ -51,22 +43,17 @@ def get_metadata(f):
 
 def run(f, reverse):
     config, instructions = get_metadata(f)
-
-    for i in instructions:
-        config = move_crate(config, *i, reverse=reverse)
-
-    sol = get_top_layer(config)
+    config = move_crates(config, instructions, reverse)
+    sol = "".join([i.pop() for i in config])
     return sol
 
 
 sol = run("05/example.txt", reverse=False)
 assert sol == "CMZ"
-
 sol = run("05/example.txt", reverse=True)
 assert sol == "MCD"
 
 sol = run("05/input.txt", reverse=False)
 print(f"The solution to the first problem is {sol}.")
-
 sol = run("05/input.txt", reverse=True)
-print(f"The solution to the first problem is {sol}.")
+print(f"The solution to the second problem is {sol}.")
